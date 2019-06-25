433326bcd05ee3af24cb0d71437c57f485207d78
sireler <sireler@gmail.com>
2019-06-25 20:32:00 +0300

    API login, register, logout

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/** Auth routes */
 Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+Route::middleware('auth:api')->post('/logout', 'AuthController@logout');
