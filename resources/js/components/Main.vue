99f9ab73d725361a1e94fcca81d5045c12cca6d3
sireler <sireler@gmail.com>
2019-06-25 20:57:01 +0300

    Updated main component

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -2,9 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="main-link mt-4 display-4">
-                    <router-link class="text-white" to="/login">Please Sign up or Log in</router-link>
+                <div class="mt-4 display-4">
+                    Music Service
                 </div>
+                <router-link class="text-white" to="/login">Please Sign up or Log in</router-link>
             </div>
         </div>
     </div>
