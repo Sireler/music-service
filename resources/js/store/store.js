91de60d6e5089dd75a9825d28f8dd9611f32d4b6
sireler <sireler@gmail.com>
2019-06-26 17:21:28 +0300

    Login feature

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,13 +1,40 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
+axios.defaults.baseURL = window.location.origin + '/api';
 
 export const store = new Vuex.Store({
     state: {
-
+        token: localStorage.getItem('access_token') || null,
     },
     mutations: {
+        retrieveToken(state, token) {
+            state.token = token;
+        }
+    },
+    actions: {
+        retrieveToken(context, credentials) {
 
+            return new Promise((resolve, reject) => {
+                axios.post('/login', {
+                    username: credentials.username,
+                    password: credentials.password,
+                })
+                    .then(response => {
+                        const token = response.data.access_token;
+
+                        localStorage.setItem('access_token', token);
+                        context.commit('retrieveToken', token);
+                        resolve(response);
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+
+
+        }
     }
 });
