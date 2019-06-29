592915f4e4726b7e1eacab5675d0fd8ad372c644
sireler <sireler@gmail.com>
2019-06-29 23:07:07 +0300

    Migrations

---
+++ database/migrations/2019_06_29_194439_create_songs_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->string('id', 32)->primary();
+            $table->bigInteger('album_id')->unsigned()->nullable();
+            $table->bigInteger('artist_id')->unsigned()->nullable();
+            $table->string('title');
+            $table->float('length');
+            $table->text('path');
+            $table->timestamps();
+
+            $table->foreign('album_id')
+                ->references('id')
+                ->on('albums')
+                ->onDelete('cascade');
+
+            $table->foreign('artist_id')
+                ->references('id')
+                ->on('artists')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
