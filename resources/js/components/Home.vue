48e6306cdfba860289d05a593cf7dc98c82cc48a
sireler <sireler@gmail.com>
2019-06-30 00:44:45 +0300

    Fixed nav active class

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -5,25 +5,25 @@
                 <!-- Left sidebar -->
                 <nav class="col-md-2 d-none d-md-block bg-light sidebar position-fixed">
                     <div class="sidebar-sticky">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <router-link class="nav-link active" :to="{ name: 'home' }">
+                        <ul class="nav nav-pills flex-column pt-2">
+                            <li class="nav-item active">
+                                <router-link class="nav-link" :to="{ name: 'home' }" exact-active-class="active">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                                    Home <span class="sr-only">(current)</span>
+                                    Home
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link active" :to="{ name: 'home.all' }">
+                                <router-link class="nav-link" :to="{ name: 'home.all' }" exact-active-class="active">
                                     All songs
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link active" :to="{ name: 'home.albums' }">
+                                <router-link class="nav-link" :to="{ name: 'home.albums' }" exact-active-class="active">
                                     Albums
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link active" :to="{ name: 'home.artists' }">
+                                <router-link class="nav-link" :to="{ name: 'home.artists' }" exact-active-class="active">
                                     Artists
                                 </router-link>
                             </li>
