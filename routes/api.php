f7850b5a78fe4abf985b0a3d3657e44722dc361c
Sireler <sireler@gmail.com>
2019-07-05 19:53:42 +0300

    Added admin column to users table

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('v1')->group(function() {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
     /** Auth routes */
     Route::post('/login', 'AuthController@login');
     Route::post('/register', 'AuthController@register');
