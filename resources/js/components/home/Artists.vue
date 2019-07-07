6fea850c7d4e1c6cbfc9ad914941a565bfc8454c
Sireler <sireler@gmail.com>
2019-07-07 19:21:04 +0300

    Artists component

--- resources/js/components/home/Artists.vue
+++ resources/js/components/home/Artists.vue
@@ -1,11 +1,28 @@
 <template>
     <div class="artists">
         <h2>Artists</h2>
+        <div class="row">
+            <div class="col-lg-3 col-md-4 mb-4"
+                 v-for=""
+                 @click="toArtist">
+                <div class="card artist-card">
+                    <img src="" alt="artist" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Artist</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
     export default {
-        name: "Artists"
+        name: "Artists",
+        methods: {
+            toArtist() {
+
+            }
+        }
     }
 </script>
 
