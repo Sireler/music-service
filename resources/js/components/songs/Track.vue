2971037be8dec3bba50407fba3ee3cb17b6f8f81
Sireler <sireler@gmail.com>
2019-07-09 20:42:05 +0300

    Format track duration

--- resources/js/components/songs/Track.vue
+++ resources/js/components/songs/Track.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="row track my-2 bg-primary rounded p-2 text-light">
-        <div class="col-md-2">
+        <div class="col-md-6">
             <span class="oi oi-play-circle m-1"
                   @click="play(track.id)"
                   v-show="playingId !== track.id || !playing"></span>
             <span class="oi oi-media-stop m-1"
                   @click="pause"
                   v-show="playingId === track.id && playing"></span>
+
+            <span class="track-title">{{ track.title }}</span>
         </div>
-        <div class="col-md-4">
-            {{ track.title }}
+        <div class="col-md-6">
+            <div class="track float-right">
+                {{ trackDuration }}
+            </div>
         </div>
     </div>
 </template>
@@ -24,7 +28,13 @@
             ...mapState({
                 playingId: state => state.player.trackId,
                 playing: state => state.player.play
-            })
+            }),
+            trackDuration() {
+                let minutes = Math.floor(this.track.length / 60);
+                let seconds = Math.floor(this.track.length % 60);
+
+                return minutes + ':' + seconds;
+            }
         },
         methods: {
             play(id) {
