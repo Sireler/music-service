0e5d33fb89e8bdba24744f135e9c9df29ade587a
Sireler <sireler@gmail.com>
2019-08-09 22:40:35 +0300

    Created TopNavbar component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,31 +1,7 @@
 <template>
     <div class="application">
         <!-- Navbar -->
-        <nav class="navbar navbar-expand-md bg-primary shadow-sm">
-            <div class="container">
-                <router-link class="navbar-brand text-white" to="/">Laravel</router-link>
-                <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <li v-if="!loggedIn" class="nav-item">
-                            <router-link class="text-white" :to="{ name: 'login' }">Login</router-link>
-                        </li>
-                        <li v-if="loggedIn" class="nav-item">
-                            <router-link class="text-white" :to="{ name: 'logout' }">Logout</router-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <TopNavbar></TopNavbar>
         <!-- /Navbar -->
         <!-- Router view -->
         <div class="current-view">
@@ -37,8 +13,13 @@
 </template>
 
 <script>
+    import TopNavbar from "./components/TopNavbar";
+
     export default {
         name: "App",
+        components: {
+            TopNavbar
+        },
         computed: {
             loggedIn() {
                 return this.$store.getters['auth/loggedIn'];
---
+++ resources/js/components/TopNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-expand-md bg-primary shadow-sm">
+        <div class="container">
+            <router-link class="navbar-brand text-white" to="/">Laravel</router-link>
+            <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <li v-if="!loggedIn" class="nav-item">
+                        <router-link class="text-white" :to="{ name: 'login' }">Login</router-link>
+                    </li>
+                    <li v-if="loggedIn" class="nav-item">
+                        <router-link class="text-white" :to="{ name: 'logout' }">Logout</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "TopNavbar"
+    }
+</script>
+
+<style scoped>
+    nav {
+        z-index: 10;
+    }
+</style>
