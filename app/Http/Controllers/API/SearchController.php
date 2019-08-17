a3d7032305a7e7cc8ff6b4b5574d1527f60adebd
Sireler <sireler@gmail.com>
2019-08-17 23:34:48 +0300

    API search albums

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Album;
 use App\Http\Controllers\Controller;
 use App\Song;
 use Illuminate\Http\Request;
@@ -19,14 +20,19 @@ class SearchController extends Controller
 
         $queryPart = array_shift($queryParts);
         $songs = Song::where('title', 'like', "%{$queryPart}%");
+        $albums = Album::where('name', 'like', "%{$queryPart}%");
 
         foreach ($queryParts as $queryPart) {
             $songs->orWhere('title', 'like', "%{$queryPart}%");
+            $albums->orWhere('name', 'like', "%{$queryPart}%");
         }
 
         return response()->json([
             'tracks' => $songs->limit(10)->get(),
             'tracks_count' => $songs->count(),
+
+            'albums' => $albums->limit(4)->get(),
+            'albums_count' => $albums->count()
         ]);
     }
 }
