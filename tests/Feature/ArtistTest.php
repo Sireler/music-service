3201ef2a1eb3a3030b4a611175d27f28e3fdd720
Sireler <sireler@gmail.com>
2019-08-30 20:27:15 +0300

    Created ArtistFactory, added tests

--- tests/Feature/ArtistTest.php
+++ tests/Feature/ArtistTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Artist;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ArtistTest extends TestCase
 {
@@ -20,5 +22,33 @@ class ArtistTest extends TestCase
             ->assertJsonStructure([
                 'message', 'data'
             ])->assertStatus(201);
+    }
+
+    public function testShowAction()
+    {
+        $artist = factory(Artist::class)->create();
+
+        $response = $this->json('GET', '/api/v1/artists/' . $artist->id);
+
+        $response->assertJsonFragment([
+            'name' => $artist->name
+        ]);
+    }
+
+    public function testUpdateAvatarAction()
+    {
+        $artist = factory(Artist::class)->create();
+        Storage::fake('public');
+
+        $response = $this->json('POST', '/api/v1/artists/' . $artist->id . '/update/avatar', [
+            'image' => $file = UploadedFile::fake()->image('avatar.jpg')
+        ]);
+
+        $response->assertStatus(200);
+
+        $artist = Artist::find($artist->id);
+        $path = preg_replace('/storage/', '', $artist->image);
+
+        Storage::disk('public')->assertExists($path);
     }
 }
