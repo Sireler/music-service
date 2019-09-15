74258cc6a1af9ce302533f545a2e4fb03761e2b3
Sireler <sireler@gmail.com>
2019-09-15 21:56:34 +0300

    Created ParserInterface

---
+++ app/Helpers/MetadataParsers/ParserInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Helpers\MetadataParsers;
+
+interface ParserInterface
+{
+    /**
+     * Get the track title
+     *
+     * @return string
+     */
+    public function getTitle();
+
+    /**
+     * Get the artist name
+     *
+     * @return string
+     */
+    public function getArtist();
+
+    /**
+     * Get the album name
+     *
+     * @return string
+     */
+    public function getAlbum();
+
+    /**
+     * Get the track length
+     *
+     * @return mixed
+     */
+    public function getLength();
+}
